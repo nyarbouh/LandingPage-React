dd1c4c2068d81c855597381f2fa42a3aea4dc438
nyarbouh <nyarbouh@me.com>
2019-09-09 21:43:27 +0000

    initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,26 +1,28 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Content } from "/workspace/react-hello/src/js/component/content";
+import { Card } from "./card";
+import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 //create your first component
-export class Home extends React.Component {
-	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+export const Home = () => {
+	return (
+		<React.Fragment>
+			<Navbar />
+			<Content />
+			<div className="container">
+				<div className="row">
+					<div className="card-deck">
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</div>
 			</div>
-		);
-	}
-}
+			<div className="footer">
+				<Footer />
+			</div>
+		</React.Fragment>
+	);
+};
